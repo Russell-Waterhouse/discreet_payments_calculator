a9013166957f0f9606909ca1b82e3bc9d4b9e849
Russell <russell.l.waterhouse@gmail.com>
2019-12-26 15:50:36 -0800

    added some function signatures to be completed later and the controller.py file to control calculations through command line

--- calculate_value.py
+++ calculate_value.py
@@ -8,3 +8,17 @@ def future_to_present(future_value, interest_rate, periods):
 def present_to_future(present_value, interest_rate, periods):
     return present_value * pow(1 + interest_rate, periods)
 
+
+def present_to_future_inflation(present_value, interest_rate, periods, inflation_rate):
+    # TODO: finish when I have this equation
+    return -1
+
+
+def future_to_present_inflation(future_value, interest_rate, periods, inflation_rate):
+    # TODO: finish when I have this equation
+    return -1
+
+
+def annuity_to_present():
+    # TODO: finish when I have this equation
+    return -1
